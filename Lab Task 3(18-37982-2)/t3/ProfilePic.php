446482c230f943503efccf009db1a5a327caa1ef
Shanzida Akter Shusmita <78260912+SAShusmitra@users.noreply.github.com>
2021-02-26 23:01:50 +0600

    Create ProfilePic.php

---
+++ Lab Task 3(18-37982-2)/t3/ProfilePic.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="PicUpload.php" method="post" enctype="multipart/form-data">
+  <img src="downloaded.jpg" alt="Profile Pic" width="100" height="120"><br>
+ 
+  <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
+  <input type="submit" value="Submit" name="submit">
+</form>
+</body>
+</html>
