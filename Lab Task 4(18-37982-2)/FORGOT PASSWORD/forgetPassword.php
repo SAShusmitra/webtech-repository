8c170f0dec94709db01c3c177f71ec867a8e8769
Shanzida Akter Shusmita <78260912+SAShusmitra@users.noreply.github.com>
2021-03-15 21:42:49 +0600

    Initial commit

---
+++ Lab Task 4(18-37982-2)/FORGOT PASSWORD/forgetPassword.php
@@ -0,0 +1,23 @@
+<div id="containersDiv">
+
+ 		<?php include ('C:\xampp\htdocs\Web Tech Lab\Project\header1.php');?>
+				
+
+			
+
+
+    <div>
+        <?php include ('2.php');?>
+    </div>
+					
+
+
+
+
+		
+		<?php
+		 include ('C:\xampp\htdocs\Web Tech Lab\Project\footer.php');
+		?>
+		
+
+</div>
