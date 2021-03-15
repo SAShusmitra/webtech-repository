b061fec0412d9fa5f1001385956145733ebdbbf8
Shanzida Akter Shusmita <78260912+SAShusmitra@users.noreply.github.com>
2021-03-15 21:59:54 +0600

    Initial Commit

---
+++ Lab Task 4(18-37982-2)/PROFILE PICTURE/nav.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+<body ">
+
+<!-- style="width: 300px;border-style: solid; border-width: thin; -->
+<p><b> &nbsp Account</b></p>
+<hr>
+<ul>
+	<li><a href="loggedInDashboard.php">Dashbord</a></li>
+	<li><a href="#news">View Profile</a></li>
+	<li><a href="#contact">Edit Profile</a></li>
+	<li><a href="profilePicture.php">Change profile picture</a></li>
+	<li><a href="changepass.php">Change Password</a></li>
+	<li><a href="logout.php">LogOut</a></li>
+</ul>
+
+
+</body>
+</html>
