8962aaeb6ae96628d2d0875da6e28c21de04b44a
Shanzida Akter Shusmita <78260912+SAShusmitra@users.noreply.github.com>
2021-04-16 23:26:05 +0600

    Initial Commit

---
+++ Lab Task 7(18-37982-2)/homepage.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BanglarHat | Ecommerce Wabesite </title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body style="background: white">
+     <h1> <pre>                                      BanglarHat <pre></h1>
+    <div class="container">
+    
+    <div class="navbar">
+      	<div class="logo" >
+      		<a href="homepage.php" target="_blank">
+      	<img src="picture/logo.png" width="120"></a>
+
+      	</div>
+      	
+          <nav>
+          	<ul>
+          		<li><a href="">Home</a></li>
+          		<li><a href="https://www.google.com/">Products</a></li>
+          		<li><a href="">About</a></li>
+          		<li><a href="">Contact</a></li>
+          		<li><a href="">Account</a></li>
+          		<li><a title="" href="Login.php">Login</a></li>
+				<li><a title="" href="registration.php">registration</a></li>
+          		
+          	</ul>
+          </nav>
+
+ 
+
+      </div>
+        
+        <div class="row">
+        <div class="col-2">
+            
+            <h1>Fresh  Food , Good Health </h1>
+       
+            </div>
+        
+        
+        </div>
+
+    </div>
+
+
+    <!---  product   --->
+
+    <div class="cetagories" >
+      <div class="row">
+
+         <div class="col-3">
+          <img src="Image1.jpg">
+        </div>
+
+
+
+
+
+</div>
+    </div>
+    
+      
+
+
+</body>
+</html>
